8719bfebadfdae29b43b1c93ab9cadd24031f18c
BagjaNr19 <2306075@itg.ac.id>
2026-03-03 13:25:43 +0700

    update code
    Refactored the main application structure to use HomePage instead of MyApp. Updated UI components including app bar and body layout.

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,75 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(HomePage());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    String title = 'Aplikasi Pertama Saya';
-    String name = 'Rizky Bagja N';
-    String message = 'Hello, $name! selamat datang di $title.';
-
+class HomePage extends StatelessWidget {
+  build(context) {
     return MaterialApp(
-      title: title,
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: Text(title),
+          backgroundColor: Colors.red[800],
+          leading: Icon(Icons.home),
+          title: Text('Aplikasi Pertamaku'),
         ),
-        body: Center(
-          child: Text(message),
+        body: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.all(20),
+            child: Column(
+            children: [
+              Row(children: [Icon(Icons.archive), Text("Artikel Baru")]),
+              Card(
+                child: Column(
+                  children: [
+                    Image.network('https://cdn.antaranews.com/cache/1200x800/2019/12/30/skysports-cristiano-ronaldo_4822329.jpg'),
+                    Text("Cristiano Ronaldo")
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              Row(
+                children: [
+                  Text(
+                    'Komentar',
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              ListView(
+                shrinkWrap: true,
+                children: [
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(3),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Jhon Doe'),
+                          Text('Per Fabrizio Romano, Cristiano Ronaldo tidak akan bermain di laga Al Nassr berikutnya melawan Al Ittihad pada Jumat ini biarpun tidak ada cedera atau masalah fisik apa pun. Ronaldo melanjutkan protesnya terhadap cara operasi manajemen klub-klub Saudi yang dianggap pilih kasih. Saudi Public Investment Fund (PIF) memilih untuk tidak mendatangkan pemain baru bagi Al Nassr, sementara klub saingan, Al Hilal (yang 75% dimiliki PIF) mendatangkan Karim Benzema yang baru saja mencetak hattrick di laga debutnya semalam. Saudi Pro League merespons protes Ronaldo, "Setiap klub beroperasi secara independen, dengan strategi perekrutan masing-masing. Cristiano telah memainkan peran penting dalam perkembangan dan ambisi Al-Nassr semenjak datang. Seperti kompetitor elit mana pun, dia ingin menang. Tapi tidak ada individual, tidak peduli seberapa signifikan, bisa menentukan keputusan di luar klubnya sendiri.", ')
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(3),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Jhon weeks'),
+                          Text('Pernah mengomentari isu internal Manchester United dengan pernyataan keras, menyindir klub tidak berkembang, dan merasa dikhianati., ')
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
+      ),
       ),
     );
   }
 }
-
